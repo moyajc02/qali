2d1c2b6c22caa5dce3eb3a7ac07d293ea12731bc
moya <moyajc@hotmail.com>
2012-02-08 21:53:03 -0600

    template

---
+++ test/unit/qalific/domain/somos/SomosControllerTests.groovy
@@ -0,0 +1,17 @@
+package qalific.domain.somos
+
+import grails.test.*
+
+class SomosControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
